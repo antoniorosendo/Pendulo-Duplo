4184d143e7359afe6ef038ed54e884ccc62a1b48
Antonio Rosendo <antonio.carosendo@gmail.com>
2026-01-27 12:37:24 -0300

    Teste com video

--- release/source/main.cpp
+++ release/source/main.cpp
@@ -121,12 +121,12 @@ int main(int argc, char *argv[]) {
 
     ///If on Raspberry:
     // open the default camera
-    VideoCapture capture(0);
+    // VideoCapture capture(0);
     // setting fps rate of video to grab
-    capture.set(CAP_PROP_FPS, int(9));
+    // capture.set(CAP_PROP_FPS, int(9));
 
     ///If working locally:
-    //VideoCapture capture("../videos/output.mp4");
+    VideoCapture capture("/home/antonio/Documents/GitHub/Pendulo-Duplo/build/teste.mp4");
 
 
     Mat originalFrame;
